c4cbde37c04e8e17f2594ffafa6b3feb13c70fff
hamirandal2005 <hamirandal2005@gmail.com>
2022-11-14 11:21:10 -0600

    UEPS

---
+++ TareaDeCurso/frmUEPS.cs
@@ -0,0 +1,25 @@
+﻿using Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clases;
+
+namespace pjContabilidadMetodosValuacion
+{
+    public partial class frmUEPS : Form
+    {
+        Stack<MatEntradaSalida> Compradas = new Stack<MatEntradaSalida>();
+        Stack<MatEntradaSalida> Usadas = new Stack<MatEntradaSalida>();
+        int UnidadesTotales = 0;
+        public frmUEPS()
+        {
+            InitializeComponent();
+        }
+    }
+}
